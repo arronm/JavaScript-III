9b23c765aad8ff75cc941eaedb34ad4687412cc8
Arron M <marshall.arron@gmail.com>
2019-04-03 09:36:37 -0700

    Finish  assignment

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* INEW!
+* 1. Implicit `this` will 'implicitly' point to the highest parent object from the referenced scope
+* 2. new will bind `this` to that instantiated class object
+* 3. Explicit `this` will bind to the object 'explicitly' specified through call, apply, bind
+* 4. Window if there is no implicit object binding found, `this` will point to the window object
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,30 @@
 // Principle 1
 
 // code example for Window Binding
+this.document;
 
 // Principle 2
 
 // code example for Implicit Binding
 
+
 // Principle 3
 
 // code example for New Binding
+class Person {
+  constructor (props) {
+    this.name = props.name;
+  }
+  speak() {
+    console.log(`Hello world, my name is ${this.name}.`);
+  }
+}
+
+const arronName = { name: "Arron" }
+const arron = new Person(arronName);
+arron.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+const jimbob = arron.speak.call({ name: "Jim" });
